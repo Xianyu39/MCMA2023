ce40d1e02089ebba4f60e000c8abe148d199bdc4
19952452972 <eric@qq.com>
2023-02-18 23:46:58 +0800

    eric

---
+++ means&std1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import math
+file_path = 'Problem_C_Data_Wordle.xlsx'
+df = pd.read_excel('Problem_C_Data_Wordle.xlsx')
+
+means = []
+
+for i in range(355):
+    row_data = df.iloc[i, 6:13]
+    sum = row_data[0]*1 + row_data[1]*2 + row_data[2]*3 + row_data[3]*4 + row_data[4]*5 + row_data[5]*6 + row_data[6]*7
+    ave = sum/100
+    means.append(ave)
+
+std = []
+
+for i in range(355):
+    row_data = df.iloc[i, 6:13]
+    sum = row_data[0] * (1-means[i])*(1-means[i]) + row_data[1] * (2-means[i])*(2-means[i]) + row_data[2] * (3-means[i])*(3-means[i]) + row_data[3] * (4-means[i])*(4-means[i]) + row_data[4] * (5-means[i])*(5-means[i]) + row_data[5] * (6-means[i])*(6-means[i]) + row_data[6] * (7-means[i])*(7-means[i])
+    ans = math.sqrt(sum/100)
+    std.append(ans)
+
+
+results_column = 'Stddev'
+df[results_column] = std
+with pd.ExcelWriter(file_path) as writer:
+    df.to_excel(writer, index=False)
